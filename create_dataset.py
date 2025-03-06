7567b024a951134e8f4a9576626806733cc962a4
Tom <tomlijding@gmail.com>
2025-03-06 19:16:48 +0100

    Added the notebook with the neural network!

--- create_dataset.py
+++ create_dataset.py
@@ -10,24 +10,27 @@ k = 10
 
 # Then build the vector x in the time-domain, we generate k random indices from 0-99, and fill our x vector with random values from 0-10
 
-amp = 10
-indices = random.sample(range(0,99),k)
-amps = random.sample(range(0,amp),k)
-x = np.zeros((N,))
+def buildDataSet(max_amplitude,min_sparsity,max_sparsity,vector_size,data_set_size):
+    sparse_data = np.zeros((vector_size,data_set_size)) # Initialize the sparse_data matrix
 
-for indx in range(k):
-    x[indices[indx]] = amps[indx]
+    # Iterate over the columns of the sparse_data matrix to define the data samples
+    for i in range(data_set_size):
+        sparsity = random.randint(min_sparsity,max_sparsity)
+        indices = random.sample(range(vector_size),sparsity)
+        amps = random.sample(range(max_amplitude),sparsity)
+        sparse_data[indices,i] = amps
+    
+    # Define the DFT matrix and multiply our spare_data vectors with it to find dense data
+    DFT = sp.linalg.dft(vector_size)/np.sqrt(vector_size)
+    dense_data = DFT@sparse_data
+    return dense_data,sparse_data
 
-# Then define the (unitary, note the scaling) DFT matrix
-
-F = sp.linalg.dft(N)/np.sqrt(N)
-Fstar = F.conj().T
-identity = F@Fstar
-
-# Notice that due to numerical errors, the resulting matrix is not unitary
-# Then generate h by taking the DFT of x, notice how h is now dense!
-
-h = F@x
+max_amplitude = 10
+min_sparsity = 3
+max_sparsity = 5
+vector_size = 100
+data_set_size = 1000
+dense_data, sparse_data = buildDataSet(max_amplitude,min_sparsity,max_sparsity,vector_size,data_set_size)    
 
 # This was done once, now we need to generate multiple thousands of h to train the dataset. 
 # Some questions would be:
@@ -35,3 +38,5 @@ h = F@x
 # Does the generated data need to be sparse at the same indices? (I think so, otherwise the trained decoder would not function properly)
 
 # Now train the autoencoder
+
+DFT = sp.linalg.dft(vector_size)/np.sqrt(vector_size)
