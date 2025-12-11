2f13cb57bab822c6b7d1de473e4a3769f8880c28
tomlijding <tomlijding@gmail.com>
2025-12-11 14:14:21 +0100

    Mini edit

--- src/comparison.py
+++ src/comparison.py
@@ -29,20 +29,20 @@ model_path= "models/sparse_recovery_model.pth"
 
 noise_levels = [2, 5, 8, 11, 14, 17, 20]
 sensing_sizes = [5, 10, 20, 30, 40, 50]
+imb_percentage_list = [0, 0.04, 0.1, 0.3, 0.6, 1]
 
 noisy_pretrained_models, imbalanced_pretrained_models, measurement_pretrained_models = load_pretrained_models()
 all_imbalanced_losses, all_measurement_losses, SNR, IRR_ratios, measurement_sizes, all_noisy_losses = evaluate_pretrained_models(noisy_pretrained_models, imbalanced_pretrained_models, measurement_pretrained_models)
-imb_percentage_list = [0, 0.04, 0.1, 0.3, 0.6, 1]
+
 xi_list = calc_xi_from_imb_percentage(imb_percentage_list)
-print(xi_list)
 loss_fn = nn.MSELoss()
 OMP_noisy_losses = []
 PSOMP_noisy_losses = []
 for noise_level in noise_levels:
     variance = 133 / (10 ** (noise_level / 10))
     hs, xs = build_dataset(config)
-    norm_losses_omp = np.zeros(config.dataset_size)
-    norm_losses_psomp = np.zeros(config.dataset_size)
+    norm_losses_omp = np.zeros(config.dataset_size,dtype=complex)
+    norm_losses_psomp = np.zeros(config.dataset_size,dtype=complex)
     for i in range(config.dataset_size):
         h = hs[i].reshape(config.dataset_size, 1)
         Phi = generate_random_phase_matrix(sensing_matrix_rows,config.vector_size)
@@ -62,7 +62,6 @@ for noise_level in noise_levels:
         norm_losses_omp[i] =  np.squeeze(NSE_OMP)
         norm_losses_psomp[i] = np.squeeze(NSE_PSOMP)
 
-
     OMP_NMSE = np.sum(norm_losses_omp)/config.dataset_size
     OMP_noisy_losses.append(OMP_NMSE)
     PSOMP_NMSE = np.sum(norm_losses_psomp)/config.dataset_size
@@ -73,8 +72,8 @@ PSOMP_imbalanced_losses = []
 for xi in xi_list:
     variance = 133 / (10 ** (noise_level / 10))
     hs, xs = build_dataset(config)
-    norm_losses_omp = np.zeros(config.dataset_size)
-    norm_losses_psomp = np.zeros(config.dataset_size)
+    norm_losses_omp = np.zeros(config.dataset_size,dtype=complex)
+    norm_losses_psomp = np.zeros(config.dataset_size,dtype=complex)
     for i in range(config.dataset_size):
         h = hs[i].reshape(config.dataset_size, 1)
         Phi = generate_random_phase_matrix(sensing_matrix_rows, config.vector_size)
@@ -103,8 +102,8 @@ OMP_sensing_size_losses = []
 PSOMP_sensing_size_losses = []
 for sensing_size in sensing_sizes:
     hs, xs = build_dataset(config)
-    norm_losses_omp = np.zeros(config.dataset_size)
-    norm_losses_psomp = np.zeros(config.dataset_size)
+    norm_losses_omp = np.zeros(config.dataset_size,dtype=complex)
+    norm_losses_psomp = np.zeros(config.dataset_size,dtype=complex)
     for i in range(config.dataset_size):
         h = hs[i].reshape(config.dataset_size, 1)
         Phi = generate_random_phase_matrix(sensing_size, config.vector_size)
@@ -134,7 +133,7 @@ fig1, (ax1, ax2, ax3) = plt.subplots(ncols=3, nrows=1, figsize=(18, 6))
 
 ax1.plot(SNR.keys(), all_noisy_losses[1], marker="o")
 ax1.plot(SNR.keys(), OMP_noisy_losses, marker="o")
-# ax1.plot(SNR.keys(), PSOMP_noisy_losses, marker="o")
+ax1.plot(SNR.keys(), PSOMP_noisy_losses, marker="o")
 ax1.set_xlabel("SNR $(dB)$")
 ax1.set_ylabel("NMSE")
 ax1.set_title("Noisy Model Performance")
@@ -144,7 +143,7 @@ ax1.legend(["Auto-encoder", "OMP", "PSOMP"])
 
 ax2.plot(IRR_ratios.values(), all_imbalanced_losses[1], marker='s')
 ax2.plot(IRR_ratios.values(), OMP_imbalanced_losses, marker="s")
-# ax2.plot(IRR_ratios.values(), PSOMP_imbalanced_losses, marker="s")
+ax2.plot(IRR_ratios.values(), PSOMP_imbalanced_losses, marker="s")
 ax2.set_xlabel("IRR $(dB)$")
 ax2.set_title("IQ Imbalanced Model Performance")
 ax2.legend(["Auto-encoder", "OMP", "PSOMP"])
@@ -153,7 +152,7 @@ ax2.grid(True)
 
 ax3.plot(measurement_sizes, all_measurement_losses[1], marker='^')
 ax3.plot(measurement_sizes, OMP_sensing_size_losses, marker="^")
-# ax3.plot(measurement_sizes, PSOMP_sensing_size_losses, marker="^")
+ax3.plot(measurement_sizes, PSOMP_sensing_size_losses, marker="^")
 ax3.set_xlabel("Measurement Dimension")
 ax3.set_title("Measurement Model Performance")
 ax3.legend(["Auto-encoder", "OMP", "PSOMP"])
